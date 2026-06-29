391f18a48f5cb77aa8271b4f5e9f54d5c661afa4
Abdullo Istamov <abdulloistamov1034@gmail.com>
2026-06-29 16:55:10 +0500

    hammasi togrilandi

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,21 @@ REST_FRAMEWORK = {
 
 
 
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
+
+
+
 from datetime import timedelta
 
 SIMPLE_JWT = {
